f96c307012c2547e0795fb588bf163dd69fdc992
Matthew O. Smith <matt@m0smith.com>
2018-01-12 20:51:43 -0700

    add index to table

--- src/main/sql/create_tables.sql
+++ src/main/sql/create_tables.sql
@@ -3,7 +3,8 @@ CREATE TABLE user_info (
        user_id VARCHAR(100),
        ward VARCHAR(20),
        user_type VARCHAR(10),
-       PRIMARY KEY (user_info_id)	
+       PRIMARY KEY (user_info_id),
+       INDEX `user_info_FI_1` (`user_id`)
 );
 
 
@@ -11,5 +12,6 @@ CREATE TABLE user_chapters (
        user_chapters_id int NOT NULL AUTO_INCREMENT,
        user_id VARCHAR(100),
        chapter VARCHAR(20),
-       PRIMARY KEY (user_chapters_id)
+       PRIMARY KEY (user_chapters_id),
+       INDEX `user_chapters_FI_1` (`user_id`)
 );
